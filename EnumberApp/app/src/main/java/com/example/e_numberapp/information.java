1d3701d372f06de4e9ce6861de8d6503b8a47e3e
MK16020 <61877101+MK16020@users.noreply.github.com>
2021-01-20 20:28:12 +0300

    maryam

--- EnumberApp/app/src/main/java/com/example/e_numberapp/information.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/information.java
@@ -13,6 +13,6 @@ public class information extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        return inflater.inflate(R.layout.fragment_information, container, false);
     }
 }
