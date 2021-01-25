752c11df973aae115b7beaedb1ea209c71c1a54e
FatimahAlbutayan <77122979+FatimahAlbutayan@users.noreply.github.com>
2021-01-25 13:33:48 +0300

    FatimahAlbutayan

--- EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class search extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
 
 }
