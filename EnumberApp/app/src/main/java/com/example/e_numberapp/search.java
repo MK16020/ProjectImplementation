30676fd131fa7781d91fc72e1ef47dffa503ef9b
FatimahAlbutayan <77122979+FatimahAlbutayan@users.noreply.github.com>
2021-01-31 11:34:23 +0300

    FatimahAlbutayan

--- EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,16 +17,18 @@ import java.util.ArrayList;
 public class search extends Fragment {
 
     
-    ListView mylistview;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
 
+        ListView  mylistview= (ListView) findViewById(R.id.listView);
+
+
         ArrayList<String> myarraylist = new ArrayList<>();
         ArrayAdapter<String> myarrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myarraylist);
-        mylistview =(ListView) findViewById(R.id.listView);
+
         mylistview.setAdapter(myarrayadapter);
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
