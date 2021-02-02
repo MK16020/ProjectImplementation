bd5b7340ad09aab5e04e0990dc27c6425947918d
FatimahAlbutayan <77122979+FatimahAlbutayan@users.noreply.github.com>
2021-02-02 16:56:24 +0300

    FatimahAlbutayan

--- EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/search.java
@@ -11,32 +11,76 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+<<<<<<< Updated upstream
 /*import com.google.firebase.*;
 import java.util.ArrayList;*/
+=======
+import com.google.firebase.*;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+>>>>>>> Stashed changes
 
 public class search extends Fragment {
 
-    
+    DatabaseReference reference;
+    ListView  mylistview;
+    ArrayList<String> myarraylist;
+    ArrayAdapter<String> myarrayadapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
         /*ArrayList<String> myarraylist = new ArrayList<>();
 =======
         ListView  mylistview= (ListView) findViewById(R.id.listView);
+=======
+>>>>>>> Stashed changes
 
+        mylistview=(ListView) findViewById(R.id.listView);
+        myarraylist = new ArrayList<>();
+        myarrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myarraylist);
+        mylistview.setAdapter(myarrayadapter);
 
+<<<<<<< Updated upstream
         ArrayList<String> myarraylist = new ArrayList<>();
 >>>>>>> 30676fd131fa7781d91fc72e1ef47dffa503ef9b
         ArrayAdapter<String> myarrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myarraylist);
+=======
+>>>>>>> Stashed changes
 
-        mylistview.setAdapter(myarrayadapter);
 
+<<<<<<< Updated upstream
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         reference.addValueEventListener(new ValueEventListener);*/
+=======
+
+
+
+
+
+
+        reference = FirebaseDatabase.getInstance().getReference();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+>>>>>>> Stashed changes
     }
 
 
