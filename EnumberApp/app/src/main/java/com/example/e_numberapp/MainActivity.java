49b15401979116f4651e3bf8547169175c90f416
ZainabAdel371 <77022435+ZainabAdel371@users.noreply.github.com>
2021-03-21 17:46:12 -0800

    zainab

--- EnumberApp/app/src/main/java/com/example/e_numberapp/MainActivity.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/MainActivity.java
@@ -22,12 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new information()).commit();*/
-
-
-
         button= (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,53 +30,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button1= (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity3();
-
-            }
-        });
-
     }
     public void openActivity2(){
         Intent intent = new Intent(this,Activity2.class);
         startActivity(intent);
     }
-    public void openActivity3(){
-        Intent intent = new Intent(this,Activity2.class);
-        startActivity(intent);
-    }
 
-    /*private BottomNavigationView.OnNavigationItemSelectedListener  navigationItemSelectedListener =new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            Fragment selectedFragment=null;
-            switch (item.getItemId()){
-
-                case R.id.information:
-                    selectedFragment=new information();
-                    break;
-
-                case R.id.camera:
-                    selectedFragment=new camera();
-                    break;
-
-                case R.id.search:
-                    selectedFragment=new search();
-                    break;
-
-                case R.id.history:
-                    selectedFragment=new history();
-                    break;
-
-            }
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
-            return true;
-        }
-    };*/
 }
 
