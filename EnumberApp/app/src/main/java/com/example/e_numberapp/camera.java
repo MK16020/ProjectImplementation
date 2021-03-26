f66e6424b918f4dfb9445fd29149d26ca8315edc
ZainabAdel371 <77022435+ZainabAdel371@users.noreply.github.com>
2021-03-25 20:34:05 -0800

    zainab

--- EnumberApp/app/src/main/java/com/example/e_numberapp/camera.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/camera.java
@@ -1,22 +1,129 @@
 package com.example.e_numberapp;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class camera extends Fragment {
 
+    private Button captureImageBtn, detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+    Bitmap imageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.fragment_camera, container, false);
+        View v = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        captureImageBtn = (Button) v.findViewById(R.id.capture_image_btn);
+        detectTextBtn = (Button) v.findViewById(R.id.detect_text_image_btn);
+        imageView = (ImageView) v.findViewById(R.id.image_view);
+        textView =(TextView) v.findViewById(R.id.text_display);
+
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakePictureIntent();
+                textView.setText("");
+            }
+        });
+
+        detectTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectTextFromImage ();
+            }
+        });
+
+        return v;
 
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextFromImage()
+    {
+        FirebaseVisionImage firrebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firrebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextFromImage(firebaseVisionText);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                Log.d("Error:" , e.getMessage());
+            }
+        });
+    }
+
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText)
+    {
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if(blockList.size() == 0 ){
+            Toast.makeText(getActivity(), "No text foung in image " , Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            for(FirebaseVisionText.Block block: firebaseVisionText.getBlocks()){
+
+                String text = block.getText();
+                textView.setText(text);
+
+            }
+        }
+    }
+
 }
 
 
