875d36487ba2c27284dcd24c3e2c968c7333b2d2
ZainabAdel371 <77022435+ZainabAdel371@users.noreply.github.com>
2021-03-25 17:24:34 -0800

    zainab

--- EnumberApp/app/src/main/java/com/example/e_numberapp/camera.java
+++ EnumberApp/app/src/main/java/com/example/e_numberapp/camera.java
@@ -1,69 +1,28 @@
 package com.example.e_numberapp;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import java.util.Objects;
-
 
 public class camera extends Fragment {
 
-    private ImageView imageView;
-    private Button button;
-    TextView t1;
-
-
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_camera, container, false);
-        imageView = (ImageView) v.findViewById(R.id.capturedImage);
-        button = (Button) v.findViewById(R.id.openCamera);
-        t1 = (TextView) v.findViewById(R.id.textView2);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent open_Camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(open_Camera, 100);
-
-            }
-        });
-        return v;
+        return  inflater.inflate(R.layout.fragment_camera, container, false);
 
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        assert data != null;
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(photo);
-
-
-    }
-
 }
+
+
+
+
+
 
 
 
